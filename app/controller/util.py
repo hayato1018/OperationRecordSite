b679049d3f83ef2e9be695076a1d60b0026e626a
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-09-21 06:56:48 +0900

    〇月1日の部分を01として列検索、年月をY_M形式に変更

--- app/controller/util.py
+++ app/controller/util.py
@@ -2,6 +2,7 @@ import csv
 from django.http import HttpResponse
 from ..models import MasterData
 import openpyxl
+from datetime import datetime
 
 def export_master_data_to_csv():
     # CSVのHTTPレスポンスを作成
@@ -18,9 +19,23 @@ def export_master_data_to_csv():
 
     return response
 
-def operation_record_export(input_book, year_month):
+def search_start_date_col(ws, keyword):
+    result = 7
+    for col in ws.columns:
+        for cell in col:
+            try:
+                value = str(cell.value)
+            except:
+                continue
+            if value == keyword:
+                retult = cell.column
+    return result
+
+def operation_record_export(input_book, year_month_str):
     wb = openpyxl.load_workbook(input_book, data_only=True)
     ws = wb['澤村']
-    print(year_month)
 
     # TODO ここに処理を追加
+    year_month = datetime.strptime(year_month_str, '%Y-%m').strftime('%Y_%m')
+    start_date_col = search_start_date_col(ws, '01')
+    print(year_month, start_date_col)
